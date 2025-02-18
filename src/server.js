2e12aa6905d3ff8ed82179d123e561b1c1b6c9c0
ivan <ivanmarti.alves@gmail.com>
2025-02-18 12:31:31 -0300

    ajustando o packge.json

--- src/server.js
+++ src/server.js
@@ -19,5 +19,5 @@ app.use((req, res, next) => {
 });
 app.use(routes);
 
-const PORT = process.env.SERVER_PORT || 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Servidor rodando na porta http://localhost:${PORT}`));
