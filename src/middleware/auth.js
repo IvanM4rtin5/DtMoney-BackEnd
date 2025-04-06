ea0402fdf97528ef13fd469846f43a90e790ef2f
Ivan Martins <160087356+IvanM4rtin5@users.noreply.github.com>
2025-04-05 21:38:24 -0300

    Update auth.js

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -2,22 +2,21 @@ const jwt = require('jsonwebtoken');
 const authConfig = require('../config/auth');
 
 module.exports = (req, res, next) => {
-  // Verifica se o cabeçalho de autorização existe
+ 
   const authorizationHeader = req.headers.authorization;
 
-  // Log no ambiente de desenvolvimento apenas
+
   if (authorizationHeader && process.env.NODE_ENV !== 'production') {
     console.log('Authorization Header:', authorizationHeader);
   }
 
-  // Verifica se o token foi fornecido
+ 
   const token = authorizationHeader?.split(' ')[1];
   if (!token) {
     return res.status(401).json({ error: 'Token não fornecido' });
   }
 
   try {
-    // Verifica o token
     const decoded = jwt.verify(token, authConfig.jwt.secret);
     req.userId = decoded.id;
     next();
